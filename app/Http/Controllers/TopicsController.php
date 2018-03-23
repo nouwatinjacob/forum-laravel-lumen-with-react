bd38035bf13db2540a442b7fadcbb82742a7729e
Nouwatin Jacob <jaysansa@gmail.com>
2018-03-24 00:16:08 +0100

    create topic and topic show

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,23 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use App\Topic;
 use Illuminate\Http\Request;
+use App\Topic;
+use App\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
   public function __construct()
   {
-    $this->middleware('auth', ['except' => 'getAllTopics']);
+    $this->middleware('auth', ['only' => 'create']);
   }
 
 
   public function getAllTopics()
   {
     $topics = Topic::orderBy('created_at', 'desc')->paginate(5);
-    $categories = Category::all();
+    // $categories = Category::all();   
+    return response()->json(['status' => 'success', 'topics' => $topics], 200);
+  }
 
-    return response()->json($topics, $categories);
+  public function create(Request $request)
+  {
+    $this->validate($request,[
+      'title' => 'required',
+      'category_id' => 'required',
+      'description' => 'required'
+    ]);
+      
+    $topic = Topic::create([
+      'user_id' => Auth::id(),
+      'title' => $request->title,
+      'category_id' => $request->category_id,
+      'description' => $request->description            
+  ]);
+
+    return response()->json([
+      'status' => 'success', 
+      'message' => 'Topic successfully created',
+      'topic' => $topic
+    ], 201);
   }
   
+  public function show($id)
+  {    
+    $topic = Topic::where('id', $id)->first();
+    if(!$topic){
+      return response()->json(['status' => 'error', 'message' => 'Topic not found'], 404);
+    }
+    return response()->json(['status' => 'success', 'topic' => $topic], 200);
+  }
 }
