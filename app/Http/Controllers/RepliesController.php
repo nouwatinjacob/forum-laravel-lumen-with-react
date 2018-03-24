bb445e74113130f87d798d5b5d08e8536e5dbfd4
Nouwatin Jacob <jaysansa@gmail.com>
2018-03-24 17:09:37 +0100

    Like and unlike for replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Reply;
 use App\Category;
 use App\Topic;
+use App\ReplyLike;
 use Auth;
 
 class RepliesController extends Controller
@@ -28,4 +29,71 @@ class RepliesController extends Controller
         'data' => $reply
       ], 201); 
     }
+
+
+    public function like($id)
+    {
+        $liked = ReplyLike::where('user_id', Auth::id())->where('reply_id', $id)->where('like', 1)->first();
+        $unliked = ReplyLike::where('user_id', Auth::id())->where('reply_id', $id)->where('like', 0)->first();
+
+        if($liked){
+          return response()->json(['message' => 'You currently liked this Reply'], 200);
+        }
+
+        elseif($unliked)
+        {
+            $unliked->delete();
+            ReplyLike::create([
+                'user_id' => Auth::id(),
+                'reply_id' => $id,
+                'like' => 1
+            ]);
+
+            return response()->json(['status' => 'success', 'message' => 'You now liked this Reply'], 201);
+        }
+
+        else{
+             ReplyLike::create([
+                'user_id' => Auth::id(),
+                'reply_id' => $id,
+                'like' => 1
+            ]);
+
+            return response()->json(['status' => 'success', 'message' => 'You now liked this Reply'], 201);
+        }
+    }
+
+
+    public function unlike($id)
+    {
+        $liked = ReplyLike::where('user_id', Auth::id())->where('reply_id', $id)->where('like', 1)->first();
+        $unliked = ReplyLike::where('user_id', Auth::id())->where('reply_id', $id)->where('like', 0)->first();
+
+        if($liked){
+            $liked->delete();
+
+            ReplyLike::create([
+                'user_id' => Auth::id(),
+                'reply_id' => $id,
+                'like' => 0
+            ]);
+            
+            return response()->json(['status' => 'success', 'message' => 'You now unliked this Reply'], 201);
+        }
+        elseif($unliked){
+            
+            return response()->json(['message' => 'You currently unliked this Reply'], 200);
+        }
+        else{
+            ReplyLike::create([
+                'user_id' => Auth::id(),
+                'reply_id' => $id,
+                'like' => 0
+            ]);
+            
+            return response()->json(['status' => 'success', 'message' => 'You now unliked this Reply'], 201);
+        }        
+        
+    }
+
 }
